6d83fe12b7842e2b44119fbf2f2a2333db6839ff
dackachki <skatmddnjs77@gmail.com>
2020-12-29 23:56:22 +0900

    29일

--- IT/app.js
+++ IT/app.js
@@ -15,3 +15,51 @@ function ArticleDetail__Body__init() {
 }
 
 ArticleDetail__Body__init();
+
+setInterval(function(){         
+            var timer = new Date();
+            var h = timer.getHours();
+            var m = timer.getMinutes();
+            var s = timer.getSeconds();
+ document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;},1000);
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+function setBackground(n){
+ 
+  var number = n;
+  var imgurl = "downloads/";
+  var url = imgurl+number+".jpg";
+  document.getElementById("mpart").style.backgroundImage= `url('${url}')`;
+  
+}
